36ffdc5d3b9c41e76c6d0028e4430f799972433e
xiaopeng <xiaopengmike@163.com>
2020-08-06 20:00:55 +0800

    08

--- tornado01.py
+++ tornado01.py
@@ -1,19 +1,16 @@
-import tornado.httpserver
 import tornado.ioloop
-import tornado.options
 import tornado.web
 
-from tornado.options import define, options
-define("port", default=8000, help="run on the given port", type=int)
-
-class IndexHandler(tornado.web.RequestHandler):
+class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        greeting = self.get_argument('greeting', 'Hello')
-        self.write(greeting + ', friendly user!')
+        self.write("Hello, world")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
 
 if __name__ == "__main__":
-    tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
-    http_server = tornado.httpserver.HTTPServer(app)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()

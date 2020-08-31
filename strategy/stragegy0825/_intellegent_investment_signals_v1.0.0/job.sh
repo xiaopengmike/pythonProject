ee9194bd24771181ef169fc5730e0e2fa3111695
xiaopeng <xiaopengmike@163.com>
2020-08-31 11:47:38 +0800

    改需求后的7*24

--- strategy/stragegy0825/_intellegent_investment_signals_v1.0.0/job.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash  
-#日志目录
-today1=$(date +%Y%m%d_%H%M)
-/usr/bin/python3 -u /opt/intellegent_investment_signals/StrategyTrader_RC0618.py  >/logs/intellegent_investment_signals-${today1}.log 2>&1 &
